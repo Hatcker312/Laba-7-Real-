f4b563a4231d389a844030f639b5f2c616ab1549
Olexandr Borisyk <saomen289130>
2024-06-05 16:49:06 +0300

    NW

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const socialLinks = document.querySelectorAll('.social-icon');
+    const socialLinks = document.querySelectorAll('.social-icon');
 
-  socialLinks.forEach(link => {
-    link.addEventListener('click', function(event) {
-        const confirmVisit = confirm(`Are you sure you want to visit: ${this.alt}?`);
-        if (!confirmVisit) {
-            event.preventDefault(); 
-        }
+    socialLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            const confirmVisit = confirm(`Are you sure you want to visit: ${this.alt}?`);
+            if (!confirmVisit) {
+                event.preventDefault(); 
+            }
+        });
     });
-});
 });
